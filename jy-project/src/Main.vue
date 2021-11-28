4644029e3ca5573a7ed4df898d49e387ed8869c3
junyoung <whfquddlsrks@gmail.com>
2021-11-28 19:47:53 +0900

    211128_jyk_Grid 페이지 Router추가

--- jy-project/src/Main.vue
+++ jy-project/src/Main.vue
@@ -1,7 +1,11 @@
 <template>
-        <div id="app">
-            <h1>Hello world</h1>
-        </div>
+<v-app id="app">
+    <h1>Hello world</h1>
+    <div id="nav">
+    <router-link to="/Grid">Grid</router-link>
+    <router-view :key="$route.fullPath" />
+    </div>
+</v-app>
 </template>
 
 <script>
@@ -9,3 +13,26 @@ export default {
     name: "Main"
 };
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
